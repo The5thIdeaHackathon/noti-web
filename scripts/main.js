681933c3925963c447ea9c2a40c7f5c66ebfedcc
NamSu <nys6635@gmail.com>
2019-05-11 11:11:17 +0900

    add sw.js & main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const applicationServerPublicKey = '<key>';
+const applicationServerPublicKey = fuck;
 
 const pushButton = document.querySelector('.js-push-btn');
 
@@ -10,8 +10,8 @@ let swRegistration = null;
 function urlB64ToUint8Array(base64String) {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
   const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
-    .replace(/_/g, '/');
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');ㄴ
 
   const rawData = window.atob(base64);
   const outputArray = new Uint8Array(rawData.length);
@@ -20,4 +20,102 @@ function urlB64ToUint8Array(base64String) {
     outputArray[i] = rawData.charCodeAt(i);
   }
   return outputArray;
+}
+
+function updateBtn() {
+  if (Notification.permission === 'denied') {
+    pushButton.textContent = 'Push가 차단되었습니다.';
+    pushButton.disabled = true;
+    updateSubscriptionOnServer(null);
+    return;
+  }
+
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable';
+  } else {
+    pushButton.textContent = 'Enable';
+  }
+
+  pushButton.disabled = false;
+}
+
+function updateSubscriptionOnServer(subscription) {
+  // TODO: Send subscription to application server
+
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails =
+      document.querySelector('.js-subscription-details');
+
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
+}
+
+function subscribeUser() {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  })
+      .then(function(subscription) {
+        console.log(subscription, 'is onSubscribe');
+
+        updateSubscriptionOnServer(subscription);
+
+        isSubscribed = true;
+
+        updateBtn();
+      })
+      .catch(function(err) {
+        console.log('fail to onSubscribe this user :', err);
+        updateBtn();
+      });
+}
+
+function initializeUI() {
+  pushButton.addEventListener('click', function() {
+    pushButton.disabled = true;
+    if (isSubscribed) {
+      // TODO: Unsubscribe user
+    } else {
+      subscribeUser();
+    }
+  });
+
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+      .then(function(subscription) {
+        isSubscribed = !(subscription === null);
+
+        updateSubscriptionOnServer(subscription);
+
+        if (isSubscribed) {
+          console.log('user id is subscribed');
+        } else {
+          console.log('user id is not subscribed');
+        }
+
+        updateBtn();
+      });
+}
+
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('service & push is supported.');
+
+  navigator.serviceWorker.register('sw.js')
+      .then(function(swReg) {
+        console.log('service is register', swReg);
+
+        swRegistration = swReg;
+        initializeUI();
+      })
+      .catch(function(error) {
+        console.error('service is error', error);
+      });
+} else {
+  console.warn('pm was not supported');
+  pushButton.textContent = 'pm was not supported';
 }
--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
 'use strict';
+
+self.addEventListener('push', function(event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+    const title = 'webOS Push';
+    const options = {
+        body: event.data.text(),
+        icon: 'images/icon.png',
+        badge: 'images/badge.png'
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
+});
